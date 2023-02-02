ed29ccf90fd9e165cd5e522446939bb10e333350
jovi848 <j.mcintyre@senit.com>
2023-02-01 19:04:33 -0500

    add collision manager and move assets

---
+++ MAPD724-W2023-ICE-3B/Manager/ColliosionManager.swift
@@ -0,0 +1,59 @@
+//
+//  ColliosionManager.swift
+//  MAPD724-W2023-ICE-3B
+//
+//  Created by Jovi on 01/02/2023.
+//
+
+import GameplayKit
+import SpriteKit
+
+class CollisionManager{
+    
+    //ref for game controller
+    public static var gameViewController:GameViewController?
+    
+    // Utility Func
+    public static func SquaredDistance(p1:CGPoint,p2:CGPoint)->CGFloat{
+        
+        let deltaX: CGFloat = p2.x - p1.x
+        let deltaY: CGFloat = p2.y - p1.y
+        
+        return deltaX*deltaX + deltaY*deltaY
+    }
+    
+    //collsion func
+    public static func sqauredRadisCheck(scene:SKScene,obj1:GameObject,obj2:GameObject){
+        
+        let p1 = obj1.position
+        let p2 = obj2.position
+        let p1Radius = obj1.halfHeight!
+        let p2Radius = obj2.halfHeight!
+        
+        let radii = p1Radius + p2Radius
+        
+        //collision check if overlapping
+        
+        if(SquaredDistance(p1: p1, p2: p2) < radii * radii){
+            
+            //we have a collsion
+            if(!obj2.isColliding!){
+                
+                
+                switch(obj2.name){
+                case "island":
+                    print("hit island")
+                    break
+                case "cloud":
+                    print("hit cloud")
+                    break
+                default:
+                    break
+                }
+                
+                obj2.isColliding = true
+
+            }
+        }
+    }
+}
